cde6e5bc722fbf49e9dfcd1d246b107ad05666d6
전의진 <138093799+Jeonuijin@users.noreply.github.com>
2023-07-20 18:19:52 +0900

    Add files via upload

---
+++ Exception/ex03_sungjuk/SungjukTest.java
@@ -0,0 +1,32 @@
+package com.mystudy.ex03_sungjuk;
+
+import com.mystudy.ex02_myexception.JumsuOutOfvalueException;
+
+public class SungjukTest {
+
+	public static void main(String[] args) {
+		System.out.println("--- main() 시작 ----");
+		SungjukVO stu = new SungjukVO("홍길동", 100, 90, 80, 0, 0);
+		stu.setKor(999);
+		
+		System.out.println("---setEng() 처리 ---");
+		try {
+			stu.setEng(999);
+		} catch (JumsuOutOfvalueException e) {
+			// e.printStackTrace();
+			System.out.println(e.getMessage() +
+					" : 0~100 범위의 값으로 다시 입력하세요");
+		} catch (SungjukProcessException e) {
+			System.out.println("[예외발생] 성적처리중 예외발생");
+		}
+		
+		
+		System.out.println("stu : " + stu);
+		
+		
+		
+		System.out.println("--- main() 끝 ----");
+
+	}
+
+}
